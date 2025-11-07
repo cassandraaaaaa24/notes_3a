955b6767eab7fddd2bcc188ef8f83fea401d5220
cassandraaaaaa24 <tashandaantonio24@gmail.com>
2025-11-07 11:05:13 +0800

    thing

--- resources/views/notes/index.blade.php
+++ resources/views/notes/index.blade.php
@@ -1,5 +1,26 @@
 <x-layout>
+    <head>
+        <style>
+            #note {
+                border: 1px solid #ccc;
+                padding: 10px;
+                margin-bottom: 10px;
+                float: left;
+                width: 20%;
+                margin-right: 15px;
+            }
+            #submission {
+                margin-bottom: 10px;
+                border: 1px solid #ccc;
+            }
 
+            #submission div {
+                margin: 10px;
+                float: left;
+                margin-bottom: 15px;
+            }
+        </style>
+    </head>
     <x-slot:title>All Notes</x-slot:title>
 
     <h1>My Notes</h1>
@@ -8,26 +29,26 @@
         <div class="alert alert-success">{{ session('status') }}</div>
     @endif
 
-    <form action="/notes" method="POST">
+    <form id="submission" action="/notes" method="POST">
         @csrf
-
-        <div>
+        
+        <div class="form-group">
             <label for="title">Title</label>
             <input id="title" name="title" type="text" value="{{ old('title') }}">
             @error('title')<div class="error">{{ $message }}</div>@enderror
         </div>
-
-        <div>
+   
+        <div class="form-group">
             <label for="content">Content</label>
             <textarea id="content" name="content">{{ old('content') }}</textarea>
             @error('content')<div class="error">{{ $message }}</div>@enderror
         </div>
-
+        <br>
         <button type="submit">Post note</button>
     </form>
 
     @foreach($notes as $note)
-    <div style="border: 1px solid #ccc; padding: 10px; margin-top: 10px; width: 300px; float: left; margin-right: 10px;">
+    <div id="note">
         <x-note-card
             :title="$note['title']"
             :content="$note['content']"
